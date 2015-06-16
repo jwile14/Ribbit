7dbfb63e9f2e0b6703f334ba4c482cfcb2bf13b7
Jenner <wilejd@rose-hulman.edu>
2015-06-16 11:48:26 -0400

    working on messaging

--- app/src/main/java/jwile14/com/github/ribbit/MainActivity.java
+++ app/src/main/java/jwile14/com/github/ribbit/MainActivity.java
@@ -189,6 +189,10 @@ public class MainActivity extends ActionBarActivity {
                 mediaScanIntent.setData(mMediaUri);
                 sendBroadcast(mediaScanIntent);
             }
+
+            Intent recipientsIntent = new Intent(this, RecipientsActivity.class);
+            recipientsIntent.setData(mMediaUri);
+            startActivity(recipientsIntent);
         }
         else if(resultCode != RESULT_CANCELED) {
             Toast.makeText(this, R.string.general_error, Toast.LENGTH_LONG).show();
--- app/src/main/java/jwile14/com/github/ribbit/ParseConstants.java
+++ app/src/main/java/jwile14/com/github/ribbit/ParseConstants.java
@@ -5,8 +5,15 @@ package jwile14.com.github.ribbit;
  */
 public final class ParseConstants {
     // Class Names
+    public static final String CLASS_MESSAGES = "Messages";
 
     // Field Names
     public static final String KEY_USERNAME = "username";
     public static final String KEY_FRIENDS_ACTIVITY = "friendsRelation";
+    public static final String KEY_RECIPIENT_IDS = "recipientIds";
+    public static final String KEY_SENDER_ID = "senderId";
+    public static final String KEY_SENDER_NAME = "senderName";
+    public static final String KEY_FILE = "file";
+    public static final String KEY_FILE_TYPE = "fileType";
+
 }
